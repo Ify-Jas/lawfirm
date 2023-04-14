dc7d8ef4af2dd62653a23142bbeb507b7b91dc6f
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-04-14 19:51:00 +0100

    updating with frontpage design and structure

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,12 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faLocationDot} from '@fortawesome/free-solid-svg-icons'
+
 function Footer (){
     return (
         <footer>
+            <p><FontAwesomeIcon icon={faLocationDot} />Office Address: 123 Unknown Crescent, GRA, Enugu.</p>
            <a href="https://ify-jas.github.io/my_react_portfolio/"> 
-                An Ifeoma Ezeoke Company
+                © Designed by Ifeoma Ezeoke
            </a>
         </footer>
        
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,12 @@
 function Home(){
     return(
         <div className="home">
-           <h5>Home</h5>
+            <div className='introContainer'>
+                <h1>OLI & CO</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus sed turpis. Habitasse platea dictumst vestibulum rhoncus. Ornare quam viverra orci sagittis eu volutpat odio facilisis. Nibh praesent tristique magna sit amet purus gravida quis blandit. Vitae suscipit tellus mauris a.</p>
+            </div>
+            <button className='homeBtn'>Contact Us</button>
+
 
         </div>
     )
